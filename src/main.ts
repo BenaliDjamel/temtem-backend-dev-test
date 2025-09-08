4482e209d9262dbea4e16253e94fe9869b20b06d
Djamel Benali <djamelbenalidev@gmail.com>
2025-09-08 23:39:49 +0100

    feat: enable transformation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   await app.listen(configService.get('APP_PORT'));
 }
